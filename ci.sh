14bdb74d33118825a1204a5c0973f268ed54ef07
Will Sturgeon <willstrgn@gmail.com>
2023-11-17 13:53:58 -0500

    `xargs` compatibility

--- ci.sh
+++ ci.sh
@@ -6,7 +6,7 @@ nix flake show .
 nix flake check .
 
 cd examples
-find . -name 'flake.*' | xargs rm
+find . -name 'flake.*' | xargs -r rm
 for dir in $(find . -type d -mindepth 1 -maxdepth 1)
 do
   cd $dir
@@ -17,5 +17,5 @@ do
   fi
   cd ..
 done
-find . -name 'flake.*' | xargs rm
+find . -name 'flake.*' | xargs -r rm
 cd ..
